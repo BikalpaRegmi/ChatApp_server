246a7a7acddc251ee57f9aa9c0ceb305d93d92cc
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2024-09-13 09:15:54 +0545

    added gitignore

--- dbs/db.js
+++ dbs/db.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
+//database connected
 export const connectDb = async() => {
     try {
           await mongoose.connect(process.env.URI);
         console.log('mongoose conneected sucessfully')
     } catch (error) {
         console.log(error)
-    }
+    } 
 }
